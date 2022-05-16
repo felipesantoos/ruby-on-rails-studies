9fca24c79105741e362fa5b03c51dc9aca975006
felipesantoos <felipedssantos811@gmail.com>
2022-05-15 22:32:28 -0300

    Add constants explanation

---
+++ 016_constants.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Constantes são usadas para salvar valores que não devem ser alterados.
+# Se você tentar alterar, vai conseguir, mas será mostrado um alerta.
+NAME = 'Felipe da Silva Santos'
+PI = 3.1415926535
+
+puts NAME
+puts PI
