5002177caf891324430ed79a8a4f70bf86680434
felipesantoos <felipedssantos811@gmail.com>
2022-05-15 22:33:07 -0300

    Add classes and objects explanations

---
+++ 018_classes_and_objects.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Class: prototype, model, description.
+# Object: variável that implements the class.
+# Methods: actions.
+# Attributes: characteristics.
+
+# Everything in Ruby is an object!
+# You can now the object class with the .class method.
+puts 1.class
+
+# You can get the object id with the .object_id method.
+puts 'My name is Felipe!'.object_id
+puts 20.object_id
+
+puts 'Felipe'.reverse
