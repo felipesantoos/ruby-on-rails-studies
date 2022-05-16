85feb9ed6d19c0daaa87578acaf25db3d05700fe
felipesantoos <felipedssantos811@gmail.com>
2022-05-15 22:33:43 -0300

    Add class instanciation explanation

---
+++ 019_class_instanciation.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# An instanciated class if an object.
+my_first_array = [1, 3, 5]
+puts my_first_array
+
+my_second_array = Array.new # it's preferable []
+my_second_array.push(2)
+my_second_array.push(4)
+my_second_array.push(6)
